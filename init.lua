fa5a510e0942ec9d76ff3d9cbd13ab4a55c163a5
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-07-19 21:22:03 +0200

    /park

--- init.lua
+++ init.lua
@@ -15,3 +15,20 @@ minetest.register_chatcommand("spawn", {
         end
     end,
 })
+minetest.register_chatcommand("park", {
+    description = "Teleport to world spawn point",
+    privs = { interact = true },
+    func = function(name, param)
+        local player = minetest.get_player_by_name(name)
+        if player then
+            if playerinpvp[name] and playerinpvp[name] > 0 then
+                minetest.chat_send_player(name, "You are in PvP")
+                return true
+            end
+            player:set_pos({ x = 314, y = -28936.5, z = 2419 })
+            return true, "Teleported to O.P. Park"
+        else
+            return false, "Player not found"
+        end
+    end,
+})
